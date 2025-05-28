44b16af200bd81385797def5b2c7ec7b149699a9
Surya <muthusurya354@gmail.com>
2025-05-28 18:23:10 +0530

    Updated Code

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById('menu-toggle');
+    const navbar = document.getElementById('navbar');
+
+    toggle.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+});
